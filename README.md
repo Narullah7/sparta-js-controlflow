0888fbdfc178be21c59c6f8b14e5120e39f9c55a
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2018-01-09 23:43:51 +0000

    corrected any mistakes and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-hello
+# Sparta JS control-flow
+
+I compute very basic commands which the browser interacts with the user, using if statements, with the alert and prompt command.
+
+First I test whether a user should 'buy a drink', 'buy crips' or 'go home' based on the input they give.
+
+Secondly I print the name of the user.
--- main.js
+++ main.js
@@ -1,41 +1,31 @@
-// var moneyInPocket =prompt('How much do you have to spend');
-//
-// if (moneyInPocket > 10) {
-//   console.log("I can buy a drink wth lunch");
-// } else if (moneyInPocket > 5) {
-//   console.log("I can buy just some crisps");
-// } else {
-//   console.log("I cant buy anything");
-// }
+var moneyInPocket = prompt("how much money do you have");
+var eatenCrisp = prompt("have you eaten crisp (reply with true or false)")
+var PriceCrisp = prompt("Price of the crisps(if free put 0)");
 
-// var moneyInPocket = prompt("how much money do you have");
-// var eatenCrisp = prompt("have you eaten crisp (reply with true or false)")
-// var PriceCrisp = prompt("Price of the crisps(if free put 0)");
-//
-// if (moneyInPocket >10 && eatenCrisp === "true") {
-//   console.log("buy another drink");
-// } else if (moneyInPocket>5 || PriceCrisp === 0) {
-//   console.log("Buy some crisps");
-// } else {
-//   console.log("Go home");
-// }
+if (parseInt(moneyInPocket) >10 && eatenCrisp === "true") {
+  alert("buy another drink");
+} else if (parseInt(moneyInPocket)>5 && parseInt(PriceCrisp) === 0) {
+  alert("Buy some crisps");
+} else {
+  alert("Go home");
+}
 
-var name = "Tim";
+var name = prompt("Are you 'Tim' or 'Bob'");
 
-// if (name === "bob") {
-//   console.log("Hi, bob");
-// }
-// else if (name === "Tim") {
-//   console.log("Hi Tim");
-// }
+if (name === "Bob") {
+  console.log("Hi, bob");
+}
+else if (name === "Tim") {
+  console.log("Hi Tim");
+}
 
 switch (name) {
   case "Bob":
-  console.log("Hi, Bob");
+  alert("Hi, Bob");
   break;
   case "Tim":
-  console.log("Hi, Tim");
+  alert("Hi, Tim");
   break;
   default:
-  console.log("Hi, default person");
+  alert("Hi, default person");
 }
